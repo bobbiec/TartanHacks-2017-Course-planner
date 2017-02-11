31b325e6e9e32e5df61247a8417a000abf3d0b48
Bobbie Chen <bobbiec@github.com>
2017-02-10 23:47:38 -0500

    Semi-functional audit form

---
+++ web/app/audit_parse/__init__.py
@@ -0,0 +1,4 @@
+from parse import parseAudit
+
+if __name__ == "__main__":
+    pass
--- web/app/views.py
+++ web/app/views.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, request
 from app import instance
+from audit_parse import parseAudit
 
 @instance.route('/')
 @instance.route('/index')
 def index():
     return render_template('index.html')
+
+@instance.route('/audit', methods=['GET', 'POST'])
+def audit():
+    jsonOutput = ""
+    humanOutput = ""
+    if request.method == "POST":
+        jsonOutput, humanOutput = parseAudit(request.form['audit'])
+    return render_template('audit.html', text=jsonOutput)
