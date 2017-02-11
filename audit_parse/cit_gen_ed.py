0aac409e23c4a3d52efd80db519892c965bab11f
Bobbie Chen <bobbiec@github.com>
2017-02-10 21:49:54 -0500

    Added academic audit parsing, cit gen-ed scraping

---
+++ audit_parse/cit_gen_ed.py
@@ -0,0 +1,25 @@
+import re
+import requests
+
+
+cit = {
+        "ppc":"https://engineering.cmu.edu/current_students/services/general_education_requirements/general_education_2016/people_places_cultures.html",
+        "sdm":"https://engineering.cmu.edu/current_students/services/general_education_requirements/general_education_2016/social_decision_making.html",
+        "i&i":"https://engineering.cmu.edu/current_students/services/general_education_requirements/general_education_2016/innovation_internationalization.html",
+        "w&e":"https://engineering.cmu.edu/current_students/services/general_education_requirements/general_education_2016/writing_expression.html"
+       }
+
+def getCourses():
+    for category in cit:
+        print("Getting %s..." % category.upper(), end=" ")
+        r = requests.get(cit["ppc"])
+        matches = re.findall(r"\d\d-\d\d\d", r.text)
+        with open("cit_%s.csv" % category, "w") as f:
+            f.write(",".join(matches))
+        print("Done.")
+
+if __name__ == "__main__":
+    # getCourses()
+    with open("cit_w&e.csv") as f:
+        cit_we = f.read()
+        print(cit_we.split(","))
