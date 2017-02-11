e788637e48b4ab0db9723553888f59b6435559ae
David Zeng <davidzeng.42@gmail.com>
2017-02-11 12:55:11 -0500

    ajax man

--- web/app/views.py
+++ web/app/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from app import instance
 from .audit_parse import parseAudit
 from .suggestions import makeSuggestion
+import json
 
 @instance.route('/')
 @instance.route('/index', methods=['GET', 'POST'])
@@ -12,13 +13,9 @@ def index():
     if request.method == "POST":
         text = request.form['audit']
         json, humanReadable = parseAudit(text)
-    return render_template('index.html', json=json, textbox=text, suggestOutput=suggestion)
+    return render_template('index.html', json=json, textbox=text)
 
-@instance.route('/suggest', methods=['GET','POST'])
+@instance.route('/suggest', methods=['GET', 'POST'])
 def suggest():
-    json = "{}"
-    text = 'Paste your academic audit here'
-    suggestion = ''
-    if request.method == "POST":
-        suggestion  = makeSuggestion(request.form['firstCourse'], request.form['secondCourse'], request.form['thirdCourse'])
-    return render_template('index.html', json=json, textbox=text, suggestOutput=suggestion)
+    courses = request.json['info']
+    return jsonify(makeSuggestion(courses[0], courses[1], courses[2]))
